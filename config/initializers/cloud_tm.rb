f1f4b77f2485342d9334822184b3cd52ea6fb3c8
ziparo <ziparo@algorithmica.it>
2012-04-30 16:41:38 +0200

    Initialization of Cloud-TM hase been moved to application.rb after the app initializers have completed

--- config/initializers/cloud_tm.rb
+++ config/initializers/cloud_tm.rb
@@ -27,15 +27,35 @@
 ###############################################################################
 ###############################################################################
 
-begin
-  require File.join(Rails.root, 'lib', 'cloud_tm', 'framework')
-
-  # loading the Fenix Framework
-  CloudTm::Framework.init(
-    :dml => 'geograph.dml',
-    :conf => 'infinispan-conf.xml',
-    :framework => CloudTm::Config::Framework::ISPN
-  )
-rescue Exception => ex
-  Rails.logger.error "Cannot load Cloud-TM Framework: #{ex}"
-end
+#begin
+#  require File.join(Rails.root, 'lib', 'cloud_tm', 'framework')
+#
+#  #Configure jgroups to use the gossip router
+#  #Note: The gossip router is in the users db
+#
+#
+#  jgroups_conf_dir = File.join(Rails.root, "lib", "fenix","conf")
+#  # open the template file, replace the GOSSIP_ROUTER_PLACEHOLDER right value
+#  puts "********************** JGROUPS CONF DIR :  #{jgroups_conf_dir}"
+#  puts "********************** MaDMASS : "
+#  puts "********************** INSTALL OPTIONS : #{Madmass.install_options(:cluster_nodes).inspect}}"
+#  jgossip_address = "#{Madmass.install_options(:cluster_nodes)[:db_nodes].first}[12001]"
+#  puts "********************** JGROUPS GOSSIP ADDR :  #{jgossip_address}"
+#  File.open(File.join(jgroups_conf_dir,"jgroups.xml.template"), 'r') do |template|
+#    jgroups_conf = template.read.gsub("{GOSSIP_ROUTER_PLACEHOLDER}", jgossip_address)
+#    puts "changing JGROUPS addres to :  #{jgossip_address}"
+#    File.open(File.join(jgroups_conf_dir,"jgroups.xml"), 'w') do |original|
+#      original.write jgroups_conf
+#    end
+#  end
+#
+#
+#  # loading the Fenix Framework
+#  CloudTm::Framework.init(
+#    :dml => 'geograph.dml',
+#    :conf => 'infinispan-conf.xml',
+#    :framework => CloudTm::Config::Framework::ISPN
+#  )
+#rescue Exception => ex
+#  Rails.logger.error "Cannot load Cloud-TM Framework: #{ex}"
+#end

f9c3129dcfc2a44713e393d8dd7964ee6585dae8
ziparo <ziparo@algorithmica.it>
2012-06-01 02:06:32 +0200

    now binding to ip
    ... not host

--- script/cloud/set_rhq_host.rb
+++ script/cloud/set_rhq_host.rb
@@ -1,8 +1,12 @@
 # This script given a host name of the mod cluster node node and generates an appropriate httpd.conf file.
 host = %x(hostname).gsub("\n", '')
+ip=%x(ifconfig eth0 | grep inet | grep -v inet6 | cut -d ":" -f 2 | cut -d " " -f 1).gsub("\n", '')
 puts "start setting rhq host with arguments #{host} ..."
 
+
 HOST_PLACEHOLDER = "{THIS_HOST}"
+IP_PLACEHOLDER   = "{THIS_IP}"
+
 
 source_root = "/opt/rhq/rhq-agent/conf/"
 current_path = File.dirname(File.expand_path(__FILE__))
@@ -17,7 +21,8 @@ rhq_agent_conf_template = File.join("#{current_path}", 'templates', 'agent-confi
 
 # open the template file, replace the HOST_PLACEHOLDER with the host argument and override the original conf file
 File.open(rhq_agent_conf_template, 'r') do |template|
-  rhq_agent_conf_data = template.read.gsub(HOST_PLACEHOLDER, host)
+  rhq_agent_conf_data = template.read.gsub(HOST_PLACEHOLDER, host).gsub(IP_PLACEHOLDER,ip)
+
   File.open(
     rhq_agent_conf, 'w') do |original|
     original.write rhq_agent_conf_data
